4864af7adff94dabf85949dee3cd50daf8753d29
Sam Bassong <sam.bassong@gmail.com>
2021-10-24 13:50:55 -0700

    added images and structure of skills page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container h-screen lg:h-screen top-0 px-5 py-10 mx-auto text-center lg:px-20">
         <div className="flex flex-col w-full mb-20">
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mt-16 mb-4 text-white">Apps I loved building</h1>
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mt-10 mb-4 text-white">Apps I loved building</h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">A few of my passion solo and collaborative projects. Hover over a card to see details and the tech stack used for implementation. Click on card and play around with the demo!</p>
         </div>
 
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,13 +1,11 @@
-import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
-import { skills } from "../data";
+
 
 const Skills = () => {
   return (
     <section id="skills">
       <div className="container h-screen lg:h-screen top-0 px-5 py-10 mx-auto">
         <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-800 mb-4">
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
@@ -16,18 +14,134 @@ const Skills = () => {
             possimus est.
           </p>
         </div>
-        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          {skills.map((skill) => (
-            <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium text-white">
-                  {skill}
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
+
+        <p>Languages</p>
+        <ul className="m-2 flex flex-wrap">
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/Q9P8Hac.png"/>
+            JavaScript
+          </li> 
+    
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/aEjpOUs.png"/>
+            Python3
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/f8ab8PH.png"/>
+            HTML5
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/j0NoZ95.png"/>
+            CSS3
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://logodix.com/logo/542135.jpg"/>
+            SQL
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/duR0hTQ.png"/>
+            NoSQL
+          </li> 
+        </ul>
+        
+        <p>Front-end</p>
+        <ul className="m-2 flex flex-wrap">
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/WlkyT3e.png"/>
+            React
+          </li> 
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/VDXSXtU.png"/>
+            Redux
+          </li> 
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/qz4DXir.png"/>
+            Vue
+          </li> 
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/pT1baZU.jpg"/>
+            Vuex
+          </li> 
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/fOcjEPM.png"/>
+            Vuetify
+          </li> 
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/T9Q7a86.png"/>
+            TailwindsCSS
+          </li> 
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/cgV9eEc.png"/>
+            Bootstrap
+          </li> 
+        </ul>
+        
+        <p>Back-end</p>
+        <ul className="m-2 flex flex-wrap">
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/GJrnwtm.png"/>
+            NodeJS
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/5KBUmxD.png"/>
+            ExpressJS
+          </li> 
+          
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/aEjpOUs.png"/>
+            Flask
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/Q9P8Hac.png"/>
+            PostgresSQL
+          </li> 
+          
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/uJMRgGU.png"/>
+            MongoDB
+          </li> 
+        </ul>
+
+        <p>Misc</p>
+        <ul className="m-2 flex flex-wrap">
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/ZjHBxKw.png"/>
+            Github
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/a3INeUB.png"/>
+            Heroku
+          </li> 
+          
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/EAdqHYd.png"/>
+            Netlify
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/tsvrUS4.png"/>
+            Vercel
+          </li> 
+          
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/Jtz0c7W.png"/>
+            AWS S3
+          </li> 
+
+          <li className='w-14 h-14 text-md flex-col justify-around mx-3'>
+            <img className=" w-full mx-2" alt='' src="https://i.imgur.com/h4ZWbL0.jpg"/>
+            GMaps API
+          </li> 
+        </ul>
+        
+
       </div>
     </section>
   );
