cad81e98df60aedd4a3bfb0b99de5fb45ca57568
Sam Bassong <sam.bassong@gmail.com>
2021-10-24 00:37:23 -0700

    contact page isn't great but works for now

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
 
   return (
     <section id="about">
-      <div className="container mx-auto mt-36 flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container h-screen lg:h-screen top-0 mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         {/* about/personal brand statement */}
         <div className="mx-16 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 pb-10 md:mb-0 items-center text-center">
           <h1 className="text-indigo-500 title-font sm:text-4xl text-3xl mb-4 font-medium ">Hi, I'm Sam!</h1>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,18 +6,16 @@ const Projects = () => {
 
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container h-screen lg:h-screen top-0 px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Apps I've Built
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mt-16 mb-4 text-white">
+            Apps I loved building
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            A few of my passion projects. Hover over a card to see details and the tech stacked used for implementations. Click on Card and play around with the demo!
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap m-8">
           {projects.map(project => (
             <ProjectCard project={project} />
           ))}
