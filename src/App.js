17bad6ace9c361285df837e21570629e4ad2a925
Sam Bassong <sam.bassong@gmail.com>
2021-11-17 12:44:23 -0800

    portfolio should be fully responsive, commiting to deploy

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
         <Contact />
       </main>
         :
-      <main  >
+      <main className='w-full' >
         <Introduction />
         <About />
         <Projects />
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -5,15 +5,15 @@ import React from "react"
 const TopNav = () => {
 
   return (
-    <header className="bg-gray-800 sticky top-0 z-10">
-      <div className="container mx-auto flex justify-between py-4">
+    <header className="overflow-x-hidden sticky w-full top-0 z-10 bg-needle-bg bg-no-repeat bg-cover bg-top">
+      <div className="sticky w-full flex justify-between py-4 bg-indigo-900 bg-opacity-70">
         <div className="title-font font-medium text-white hover:text-yellow-400 ml-4">
-          <a href="#about" className="text-lg">Sam Bassong</a>
+          <a href="#about" className="text-xl">Sam Bassong</a>
         </div>
 
         <section className='flex mr-4'>
           <a href="https://drive.google.com/file/d/1vsehuFtOWocfYJI4TDIYDiI5lG-VR2o-/view?usp=sharing" className="mx-1 inline-flex items-center bg-blue-400 border-0 py-1 px-2 hover:bg-blue-500 text-white focus:outline-none rounded text-base" rel="noreferrer" target="_blank"> Resume <ImProfile className="w-4 h-4 ml-2" /> </a>
-          <a href="#contact" className="mx-1 inline-flex items-center bg-yellow-500 border-0 py-1 px-1 hover:bg-yellow-400 text-white focus:outline-none rounded text-base"> Hire Me! <MdOutlineEmail className="w-4 h-4 ml-2" /> </a>
+          <a href="#contact" className="mx-1 inline-flex items-center bg-yellow-500 border-0 py-1 px-2 hover:bg-yellow-400 text-white focus:outline-none rounded text-base"> Hire Me! <MdOutlineEmail className="w-4 h-4 ml-2" /> </a>
         </section>
       </div>
     </header>
