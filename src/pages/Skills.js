afbff15a83b974484c8ccfdd2f4768652201896a
Sam Bassong <sam.bassong@gmail.com>
2021-11-16 18:58:09 -0800

    Skills page now responsive

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -3,147 +3,150 @@
 const Skills = () => {
   return (
     <section id="skills">
-      <div className="container h-screen top-5 px-5 py-10 mx-auto bg-indigo-500 bg-opacity-40">
+      <div className="container h-screen  px-5 py-10 mx-auto bg-indigo-500 bg-opacity-40">
         <div className="text-center mb-12">
           <h1 className="sm:text-4xl text-3xl font-bold title-font text-white">
             Skills &amp; Technologies
           </h1>
         </div>
 
-        <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-2 rounded-lg my-2'>
-          <p className='text-yellow-400 text-xl font-bold'>Languages</p>
-          <ul className="text-center text-lg text-white flex font-medium justify-around pt-4 py-10 ">
+        <section className="container mx-auto flex flex-col justify-evenly">
+          <div className='w-full lg:w-4/5 mx-auto bg-black bg-opacity-50 p-2 rounded-lg my-2'>
+            <p className='text-yellow-400 text-lg lg:text-xl font-bold'>Languages</p>
+            <ul className="m-1 text-center text-lg text-white flex font-medium justify-around pt-4 py-10 ">
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/Q9P8Hac.png"/>
-              JavaScript
-            </li> 
-      
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/aEjpOUs.png"/>
-              Python3
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/Q9P8Hac.png"/>
+                JavaScript
+              </li> 
+        
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/aEjpOUs.png"/>
+                Python3
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/f8ab8PH.png"/>
-              HTML5
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/f8ab8PH.png"/>
+                HTML5
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/j0NoZ95.png"/>
-              CSS3
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/j0NoZ95.png"/>
+                CSS3
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://logodix.com/logo/542135.jpg"/>
-              SQL
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://logodix.com/logo/542135.jpg"/>
+                SQL
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/duR0hTQ.png"/>
-              NoSQL
-            </li> 
-          </ul>
-        </div>
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/duR0hTQ.png"/>
+                NoSQL
+              </li> 
+            </ul>
+          </div>
 
-        <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-3 rounded-lg my-2'>
-          <p className='text-yellow-400 text-xl font-bold'>Front-end</p>
-          <ul className="m-2 text-center flex justify-around text-lg text-white font-medium  pt-4 py-10">
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/WlkyT3e.png"/>
-              React
-            </li> 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/VDXSXtU.png"/>
-              Redux
-            </li> 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/qz4DXir.png"/>
-              Vue
-            </li> 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/pT1baZU.jpg"/>
-              Vuex
-            </li> 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/fOcjEPM.png"/>
-              Vuetify
-            </li> 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/T9Q7a86.png"/>
-              Tailwinds CSS
-            </li> 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/cgV9eEc.png"/>
-              Bootstrap
-            </li> 
-          </ul>
-        </div>
+          <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-3 rounded-lg my-2'>
+            <p className='text-yellow-400 text-lg lg:text-xl font-bold'>Front-end</p>
+            <ul className="m-1 text-center flex justify-around text-lg text-white font-medium  pt-4 py-10">
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/WlkyT3e.png"/>
+                React
+              </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/VDXSXtU.png"/>
+                Redux
+              </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/qz4DXir.png"/>
+                Vue
+              </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/pT1baZU.jpg"/>
+                Vuex
+              </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/fOcjEPM.png"/>
+                Vuetify
+              </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/T9Q7a86.png"/>
+                Tailwinds CSS
+              </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/cgV9eEc.png"/>
+                Bootstrap
+              </li> 
+            </ul>
+          </div>
 
-        <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-2 rounded-lg my-2'>
-          <p className='text-yellow-400 text-xl font-bold'>Back-end</p>
-          <ul className="m-2 text-center flex justify-around  text-white text-lg font-medium pt-4 py-10">
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full" alt='' src="https://i.imgur.com/GJrnwtm.png"/>
-              <p>NodeJS</p>
-            </li> 
+          <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-2 rounded-lg my-2'>
+            <p className='text-yellow-400 text-lg lg:text-xl font-bold'>Back-end</p>
+            <ul className="m-1 text-center flex justify-around  text-white text-lg font-medium pt-4 py-10">
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full" alt='' src="https://i.imgur.com/GJrnwtm.png"/>
+                <p>NodeJS</p>
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full" alt='' src="https://i.imgur.com/5KBUmxD.png"/>
-              ExpressJS
-            </li> 
-            
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/XcW6v5ym.png"/>
-              Flask
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full" alt='' src="https://i.imgur.com/5KBUmxD.png"/>
+                ExpressJS
+              </li> 
+              
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/XcW6v5ym.png"/>
+                Flask
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/JI955nx.png"/>
-              PostgresSQL
-            </li> 
-            
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/uJMRgGU.png"/>
-              MongoDB
-            </li> 
-          </ul>
-        </div>
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/JI955nx.png"/>
+                PostgresSQL
+              </li> 
+              
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/uJMRgGU.png"/>
+                MongoDB
+              </li> 
+            </ul>
+          </div>
 
-        <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-3 rounded-lg my-2'>
-          <p className='text-yellow-400 text-xl font-bold'>Misc</p>
-          <ul className="m-2 text-center flex justify-around text-lg text-white font-medium pt-4 py-10">
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/ZjHBxKw.png"/>
-              Github
-            </li> 
+          <div className='w-4/5 mx-auto bg-black bg-opacity-50 p-3 rounded-lg my-2'>
+            <p className='text-yellow-400 text-lg lg:text-xl font-bold'>Misc</p>
+            <ul className="m-1 text-center flex justify-around text-lg text-white font-medium pt-4 py-10">
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/ZjHBxKw.png"/>
+                Github
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/a3INeUB.png"/>
-              Heroku
-            </li> 
-            
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/EAdqHYd.png"/>
-              Netlify
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/a3INeUB.png"/>
+                Heroku
+              </li> 
+              
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/EAdqHYd.png"/>
+                Netlify
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/tsvrUS4.png"/>
-              Vercel
-            </li> 
-            
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/Jtz0c7W.png"/>
-              AWS S3
-            </li> 
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/tsvrUS4.png"/>
+                Vercel
+              </li> 
+              
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/Jtz0c7W.png"/>
+                AWS S3
+              </li> 
 
-            <li className='w-16 h-16 text-md flex-col mx-auto'>
-              <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/h4ZWbL0.jpg"/>
-              GMaps API
-            </li> 
-          </ul>
-        </div>
+              <li className='w-6 h-6 md:w-10 md:h-10 lg:w-14 lg:h-14 xl:w-16 xl:h-16 text-md flex-col mx-auto'>
+                <img className=" w-full h-full mb-2" alt='' src="https://i.imgur.com/h4ZWbL0.jpg"/>
+                GMaps API
+              </li> 
+            </ul>
+          </div>
+
+        </section>
 
       </div>
     </section>
